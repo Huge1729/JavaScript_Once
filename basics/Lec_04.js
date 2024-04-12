773f0f72a5cc1492dc04efdb50a0719e81110be4
Vishal Pal <118637617+Huge1729@users.noreply.github.com>
2024-04-12 10:59:45 +0000

    IIFE End!!

--- basics/Lec_04.js
+++ basics/Lec_04.js
@@ -1,12 +1,12 @@
-+++++++++++++++++++++++++  Function +++++++++++++
-function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
-}
+// +++++++++++++++++++++++++  Function +++++++++++++
+// function sayMyName(){
+//     console.log("H");
+//     console.log("I");
+//     console.log("T");
+//     console.log("E");
+//     console.log("S");
+//     console.log("H");
+// }
 // sayMyName   --> this is just refernce 
 // sayMyName()  --> this is execution of function
 
@@ -30,5 +30,86 @@ function sayMyName(){
 // handleObject(user)
 
 
-+++++++++++++++++++++ Scopes ++++++++++++++++++
+// +++++++++++++++++++++ Scopes ++++++++++++++++++
+// ********** This is function 
+// console.log(addone(5))   also can call before declaration
+// function addone(num){            
+//     return num + 1
+// }
 
+// console.log(addone(5))   also good
+
+// ******This is expression
+// addTwo(5) This is not possible
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+// addTwo(5)   Expression can called after declaration
+
+
+
+// +++++++++++++++++++++++ Arrows ++++++++++++++++++++++
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);  ----> This gives context {} But browser gives window as context of this.
+
+
+// **Arrow function
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  (num1 + num2)
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+// console.log(addTwo(3, 4))
+
+
+// ++++++++++++++++++++++++++++++ IIFE +++++++++++++++++++++++++++
+// Immediately Invoked Function Expressions (IIFE)
+
+// Chai() Analogy
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+// ####  The semicolon is must to block the prev context other wise it give error 
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh');
+
+// const counter = (function () {
+//     let count = 0;
+    
+//     return function () {
+//       count += 1;
+//       return count;
+//     };
+
+//    })();
+//    console.log(counter())  1
+//    console.log(counter())  2
+
+
+// WHY??
+// 1. One of the primary uses for an IIFE is to create private scope.
+// 2.  closures to protect variables or methods from being accessed!(Protect from the gobalPollution)
