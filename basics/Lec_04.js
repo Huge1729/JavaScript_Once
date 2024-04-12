c1f5244e437fb9eae4903e6384d04c55d41418e9
Vishal Pal <118637617+Huge1729@users.noreply.github.com>
2024-04-12 06:28:15 +0000

    Function and Scopes

---
+++ basics/Lec_04.js
@@ -0,0 +1,34 @@
++++++++++++++++++++++++++  Function +++++++++++++
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+// sayMyName   --> this is just refernce 
+// sayMyName()  --> this is execution of function
+
+// ** This is way of passing any number of parameter when you initially didn't have enough info about arg. 
+
+// function calculateCartPrice(...num1){
+    // return num1      This will store in the form of Array
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+// **  This is how object passed in function 
+// const user = {
+//     username: "hitesh",
+//     prices: 199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+// handleObject(user)
+
+
++++++++++++++++++++++ Scopes ++++++++++++++++++
+
